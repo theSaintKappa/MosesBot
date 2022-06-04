a34db88fc4dc253fe35c575a725ba4d20052eed1
Jgrtowy <73594473+Jgrtowy@users.noreply.github.com>
2022-06-04 20:10:09 +0200

    Add files via upload

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -5,5 +5,41 @@ const leaderboardSchema = require('../quote-leaderboard-schema');
 
 
 module.exports = {
-    // stuff
+    category: 'Messages leaderboard',
+    description: 'Display leaderboard of messages sent',
+
+    slash: true,
+    testOnly: true,
+
+    callback: async({ interaction }) => {
+
+        const quotesLbArray = await leaderboardSchema.find({});
+        var quotesLbString = '';
+
+        quotesLbArray.every(userName=> {
+            quotesLbString += `**#${userName['userId']}** \`${userName['count']}\`\n`;
+            if (quotesLbString.length > 4000) {
+                quotesLbString += '**+more**';
+                return false;
+            } else return true;
+        });
+
+        const viewleaderboardsEmbed = new MessageEmbed()
+            .setColor('RANDOM')
+            .setDescription(quotesLbString)
+            .setTimestamp()
+            .setFooter({ text: 'MosesDB', iconURL: 'https://cdn.discordapp.com/avatars/315531146953752578/c74e42cfa5ab08a5daa5ede7365e2244.png?size=4096' });
+
+        if (quotesString == '') {
+            viewleaderboardsEmbed.setTitle('The leaderboard is empty!');
+        } else {
+            viewleaderboardsEmbed.setTitle('Here is current state of leaderboard:');
+        }
+
+        if (interaction) {
+            interaction.reply({
+                embeds: [viewleaderboardsEmbed]
+            });
+        }
+    }
 };
