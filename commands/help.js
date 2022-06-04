351830b14fcc45bb4a4740e1dcc752978d0221cf
SaintKappa <email@example.com>
2022-06-05 00:06:36 +0200

    updated help command

--- commands/help.js
+++ commands/help.js
@@ -13,18 +13,18 @@ module.exports = {
             .setURL('https://thesaintkappa.github.io/dmqbot/')
             .setThumbnail('https://cdn.discordapp.com/attachments/980813644948463656/982458232306032700/3.jpg?size=4096')
             .setDescription('ㅤ')
-            .addField('MosesDB related commands', '╒═════════════════════════════╕', false)
+            .addField('MosesDB related commands', '╒════════════════════════╕', false)
             .addField('/add', 'Add a new quote to the MosesDB', true)
             .addField('/remove', 'Remove a specified quote from the MosesDB', true)
             .addField('/quotes', 'Display all the currently stored Moses Quotes', true)
             .addField('/leaderboard', 'Display a leaderboard of who added the most Moses quotes to the database', true)
-            .addField('╘═════════════════════════════╛', 'ㅤ', false)
-            .addField('MosesUtilities commands', '╒═════════════════════════════╕', false)
+            .addField('╘════════════════════════╛', 'ㅤ', false)
+            .addField('MosesUtilities commands', '╒════════════════════════╕', false)
             .addField('/reroll', 'Re-roll today\'s daily quote', true)
             .addField('/goodnight', 'Wish everyone a good night sleep', true)
             .addField('/status', 'Change the bot status', true)
             .addField('/help', 'Display this menu', true)
-            .addField('╘═════════════════════════════╛', 'ㅤ', false)
+            .addField('╘════════════════════════╛', 'ㅤ', false)
             .setFooter({ text: 'Bot made by: SaintKappa#7400', iconURL: 'https://cdn.discordapp.com/avatars/315531146953752578/a_15f39d41ad74001f0596bddc61c89847.png?size=4096' });
         if (interaction) {
             interaction.reply({
