68dbd0c08ff4805622ac307a7917ad64e640ef17
SaintKappa <email@example.com>
2022-10-01 11:10:20 +0200

    /reroll fix

--- commands/reroll.js
+++ commands/reroll.js
@@ -11,7 +11,9 @@ module.exports = {
     callback: async({ interaction }) => {
         const viewquotesEmbed = new EmbedBuilder()
             .setColor('Random')
-            .addField('```🔄 Re-rolled today\'s quote```', '**TODO:** Delete previous quote automatically (optional arg)', true)
+            .addFields([
+                { name: '```🔄 Re-rolled today\'s quote```', value: '**TODO:** Delete previous quote automatically (optional arg)', inline: true }
+            ])
             .setTimestamp()
             .setFooter({ text: 'Daily Moses Quote', iconURL: 'https://cdn.discordapp.com/avatars/315531146953752578/c74e42cfa5ab08a5daa5ede7365e2244.png?size=4096' });
         if (interaction) {
