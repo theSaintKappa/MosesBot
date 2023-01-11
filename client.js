3ab6a3db7608679b8dc043408a1533ab891b6692
SaintKappa <00wojtekz@gmail.com>
2023-01-11 17:29:36 +0100

    updated memes channel reaction check

--- client.js
+++ client.js
@@ -109,7 +109,8 @@ client.on(Events.MessageCreate, async (message) => {
     if (Object.values(serverChannels).includes(message.channel.id)) {
         try {
             // if message in memes channel && if message has no attachments
-            if (message.channel.id === serverChannels.memes && !message.attachments.size && !message.content.startsWith("https://cdn.discordapp.com/attachments/")) return;
+            if (message.channel.id === serverChannels.memes && !message.attachments.size && !message.content.startsWith("https://cdn.discordapp.com/attachments/") && !message.content.startsWith("https://media.discordapp.net/attachments/"))
+                return;
 
             await message.react("<:upvote:982630993997496321>");
             await message.react("<:downvote:982630978566639616>");
