b5c14e4cc23c885804083148d26cec19a2de5ac0
SaintKappa <00wojtekz@gmail.com>
2023-04-04 20:51:58 +0200

    ⏳ Add timezone to cron job

--- cron/quote-cron.js
+++ cron/quote-cron.js
@@ -1,23 +1,24 @@
-// const cron = require('node-cron');
 const cron = require('node-schedule');
 const axios = require('axios');
 const sendQuote = require('../send-quote');
 
+const rule = new cron.RecurrenceRule();
+rule.hour = process.env.QUOTE_HOUR || 7;
+rule.minute = process.env.QUOTE_MINUTE || 15;
+rule.tz = process.env.QUOTE_TZ || 'Europe/Warsaw';
+
 module.exports = (instance, client) => {
-    cron.scheduleJob(
-        '15 7 * * *', // Everyday at 7:15
-        async () => {
-            try {
-                sendQuote(client);
+    cron.scheduleJob(rule, async () => {
+        try {
+            sendQuote(client);
 
-                const moses = [...'Moses'].sort(() => 0.5 - Math.random());
-                client.guilds.cache.get('980813190780841984').members.cache.get('389021335285661707').setNickname(moses.join(''));
+            const moses = [...'Moses'].sort(() => 0.5 - Math.random());
+            client.guilds.cache.get('980813190780841984').members.cache.get('389021335285661707').setNickname(moses.join(''));
 
-                const request = await axios.get(`https://api.saintkappa.xyz/vulcan/luckyNumber`);
-                client.channels.cache.get('1067438722703953992').setName(`🍀🔢 ›› ${request.data.number ?? 'brak'}`);
-            } catch (err) {
-                console.error(err);
-            }
+            const request = await axios.get(`https://api.saintkappa.xyz/vulcan/luckyNumber`);
+            client.channels.cache.get('1067438722703953992').setName(`🍀🔢 ›› ${request.data.number ?? 'brak'}`);
+        } catch (err) {
+            console.error(err);
         }
-    );
+    });
 };
