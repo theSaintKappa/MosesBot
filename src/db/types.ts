69ceded8952430ee7d18770d25428e08ff8f4a8e
Wojtek Zrałek <00wojtekz@gmail.com>
2023-12-29 01:06:10 +0100

    ⌚ Add voice time tracking (testing phase)

--- src/db/types.ts
+++ src/db/types.ts
@@ -1,4 +1,4 @@
-import { ActivityType, ClientPresenceStatus } from "discord.js";
+import { ActivityType, ClientPresenceStatus, Snowflake } from "discord.js";
 import { Document } from "mongoose";
 
 export interface DocumentTimestamps {
@@ -6,25 +6,27 @@ export interface DocumentTimestamps {
     updatedAt: Date;
 }
 
-export interface IMosesQuote extends Document, DocumentTimestamps {
+interface IQuote extends Document, DocumentTimestamps {
     id: number;
     content: string;
-    submitterId: string;
+    submitterId: Snowflake;
 }
 
-export interface IPtQuote extends IMosesQuote {
-    authorId: string;
+export interface IMosesQuote extends IQuote {}
+
+export interface IPtQuote extends IQuote {
+    authorId: Snowflake;
 }
 
 export interface ILeaderboard extends Document, DocumentTimestamps {
-    userId: string;
+    userId: Snowflake;
     count: number;
 }
 
 export interface IMosesPic extends Document, DocumentTimestamps {
     id: string;
     url: string;
-    submitterId: string;
+    submitterId: Snowflake;
     name: string;
     size: number;
     dimensions: { width: number; height: number };
@@ -32,14 +34,19 @@ export interface IMosesPic extends Document, DocumentTimestamps {
 }
 
 export interface IMosesPicUploader extends Document, DocumentTimestamps {
-    userId: string;
-    adminId: string;
+    userId: Snowflake;
+    adminId: Snowflake;
 }
 
 export interface IPresence extends Document, DocumentTimestamps {
     type: ActivityType;
     name: string;
     status: ClientPresenceStatus;
+}
+
+export interface IVoiceTime extends Document, DocumentTimestamps {
+    userId: string;
+    time: number;
 }
 
 export interface SchemaWithMetadata<T> {
---
+++ src/models/bot/voiceTime.ts
@@ -0,0 +1,12 @@
+import { Schema, model } from "mongoose";
+import { IVoiceTime } from "../../db/types";
+
+const schema = new Schema<IVoiceTime>(
+    {
+        userId: { type: String, required: true },
+        time: { type: Number, required: true },
+    },
+    { timestamps: true, versionKey: false }
+);
+
+export default model<IVoiceTime>("bot.voiceTime", schema, "bot.voiceTime");
